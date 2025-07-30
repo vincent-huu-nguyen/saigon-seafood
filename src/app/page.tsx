392d67bcdabd290509ede00ecff17b76c43635c8
vinsanite <vincentnguyen7011@gmail.com>
2025-07-29 23:27:20 -0500

    moved home content up slightly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-8 bg-white text-gray-800">
       <motion.div
-        className="max-w-xl text-center"
+        className="max-w-xl text-center -mt-50"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.4 }}>
